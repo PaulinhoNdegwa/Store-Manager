f82a5edcb436741ef10b6c89ede06b2a9d1e9d36
PaulinhoNdegwa <pherndegz@gmail.com>
2018-11-12 21:48:19 +0300

    [Chore #161866703] - Refactor HTML scripts to suit backend logic

--- js/main.js
+++ js/main.js
@@ -1,50 +1,97 @@
-	function toogleMenu() {
-	    var nav = document.getElementById("myNavbar");
-	    if (nav.className === "navbar") {
-	        nav.className += " responsive";
-	    } else {
-	        nav.className = "navbar";
-	    }
+// const api_auth_url = 'http://localhost:5000/api/v2/auth';
+// const api_url = 'http://localhost:5000/api/v2';
+
+const api_auth_url = 'https://storemanager-paul-v2.herokuapp.com/api/v2/auth';
+const api_url = 'https://storemanager-paul-v2.herokuapp.com/api/v2';
+
+
+function toogleMenu() {
+	var nav = document.getElementById("myNavbar");
+	if (nav.className === "navbar") {
+		nav.className += " responsive";
+	} else {
+		nav.className = "navbar";
 	}
+}
 
-function deleteModal(){
+function deleteModal(id, user) {
 	delete_modal.style.display = "block";
+	window.localStorage.setItem('delete_id', id)
+	document.getElementById("to_delete").innerHTML = user
 }
 
-function closeModal(){
+function closeModal() {
 	delete_modal.style.display = "none";
+	window.localStorage.removeItem('delete_id')
 }
-function att_login_form_modal(){
+function att_login_form_modal() {
 	attendant_form_modal.style.display = "block";
 }
 
-function pdt_new_form_modal(){
+function pdt_new_form_modal() {
 	pdt_form_modal.style.display = "block";
 }
-function admin_login_form_modal(){
-	admin_form_modal.style.display = "block";
+
+function pdt_edit_form_modal(id, pdt_name, pdt_model, price) {
+	pdt_form_modal.style.display = "block";
+
+	window.localStorage.setItem('edit_id', id)
+	document.getElementById('title').innerHTML = "Edit product details";
+	title.style.color = "red"
+	document.getElementById('name').value = pdt_name;
+	document.getElementById('model').value = pdt_model;
+	document.getElementById('price').value = price;
+	document.getElementById('savepdt').value = "Edit Product";
+	document.getElementById('savepdt').setAttribute("onclick", `products.edit_product('${id}')`)
 }
 
-function close_form_modal(){
+function close_form_modal() {
 	attendant_form_modal.style.display = "none";
-	admin_form_modal.style.display = "none";
+	pdt_form_modal.style.display = "none";
+	window.localStorage.removeItem('edit_id')
+}
+function close_pdt_modal() {
 	pdt_form_modal.style.display = "none";
 }
-function close_pdt_modal(){
-	pdt_form_modal.style.display = "none";
-}
-function make_admin_modal(){
+function make_admin_modal(id, user) {
 	admin_modal.style.display = "block";
+	window.localStorage.setItem('edit_id', id)
+	document.getElementById("user_to_edit").innerHTML = user
 }
 
-function close_make_admin_modal(){
+function close_make_admin_modal() {
 	admin_modal.style.display = "none";
 }
 
-function att_new_form_modal(){
+function att_new_form_modal() {
 	att_form_modal.style.display = "block";
 }
 
-function close_att_form_modal(){
+function close_att_form_modal() {
 	att_form_modal.style.display = "none";
+}
+
+function set_cart_total() {
+	cart_items = localStorage.getItem('cart_total');
+	console.log(cart_items)
+	if (cart_items == null) {
+		localStorage.setItem('cart_total', 0);
+	}
+	cart_span = document.getElementById('cart-span');
+	cart_items = localStorage.getItem('cart_total');
+	// cart_span.insertAdjacentHTML('beforeend', cart_items)
+	cart_span.innerHTML = cart_items
+}
+
+
+function display_error(data) {
+	const error = document.getElementById("error")
+	error.innerHTML = data
+	error.style.display = "block"
+}
+
+function display_form_error(data) {
+	const error = document.getElementById("error-form")
+	error.innerHTML = data
+	error.style.display = "block"
 }
